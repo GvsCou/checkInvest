162fa9be9b88f3c7b668d2af4378ec6207ec43a3
GvsCou <gvscou@protonmail.com>
2021-02-27 19:33:08 -0300

    Fixed overwrite by checking if file is empty, i. e., has 0 bytes , which I think can lead to some troubles in the future

--- entry.py
+++ entry.py
@@ -9,26 +9,37 @@ from paths import paths
 class DICT_MODES(enum.Enum):
 	OLD = 0
 	NEW = 1
+	BRAND_NEW = 2
+
 
 def new_dict(mode: int, price: float, quantity: float, ticker: str="") -> dict:
 	c_dict: dict = {}
 
-	if mode == DICT_MODES.NEW:
+	if mode == DICT_MODES.OLD:
+		c_dict = {
+			"price": price,
+			"quantity": quantity,
+			"date": str(datetime.date.today())
+		}
+
+	elif mode == DICT_MODES.NEW:
 		c_dict = { 
-			ticker: {
+			"entry_1": {
+				"price": price,
+				"quantity": quantity,
+				"date": str(datetime.date.today())
+			}
+		}
+	
+	elif mode == DICT_MODES.BRAND_NEW:
+		c_dict = {
+			ticker: { 
 				"entry_1": {
 					"price": price,
 					"quantity": quantity,
 					"date": str(datetime.date.today())
 				}
 			}
-		}
-
-	elif mode == DICT_MODES.OLD:
-		c_dict = {
-			"price": price,
-			"quantity": quantity,
-			"date": str(datetime.date.today())
 		}
 	
 	return c_dict
@@ -46,14 +57,19 @@ def dump_json(path: str, py_dict: dict, indentation: int=2):
 	json.dump(py_dict, w_file, indent=indentation)
 
 
-def add_new(ticker: str):
+def add_new(ticker: str, mode: int=DICT_MODES.NEW):
 	price: float = float(input("Enter the price of the asset: "))
 	quantity: float = float(input("Enter the quantity of the asset: "))
 	
-	new_entry: dict = new_dict(DICT_MODES.NEW, price, quantity, ticker)
-	
-	dump_json(paths['data_file'], new_entry)
+	if mode == DICT_MODES.NEW:
+		new_entry: dict = new_dict(mode, price, quantity)
+		old_entry: dict = get_json(paths['data_file'])	
+		old_entry[ticker] = new_entry
+		dump_json(paths['data_file'], old_entry)
 
+	elif mode == DICT_MODES.BRAND_NEW:
+		new_entry: dict = new_dict(mode, price, quantity, ticker)
+		dump_json(paths['data_file'], new_entry)
 
 def add_to_old(ticker: str):
 	price: float = float(input("Enter the price of the asset: "))
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 
-import sys, entry
+import sys, entry, os
 from paths import paths
 
 def switch(option: str):
 	def add_entry():
 		ticker: str = input("Enter asset name: ")
-		py_dict: dict = entry.get_json(paths['data_file'])
+
+		if os.stat(paths['data_file']).st_size == 0:
+			entry.add_new(ticker, entry.DICT_MODES.BRAND_NEW)
+			return None	
+		else:
+			py_dict: dict = entry.get_json(paths['data_file'])
 		
-		for key in py_dict:
-			if key == ticker:
-				entry.add_to_old(ticker)
-				return None
+			for key in py_dict:
+				if key == ticker:
+					entry.add_to_old(ticker)
+					return None
 		
-		entry.add_new(ticker)
+			entry.add_new(ticker)
 
 	def default():
 		print("Invalid Option")
