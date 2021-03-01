a46e865faabba082852aba02d5209d3799778a63
GvsCou <gvscou@protonmail.com>
2021-03-01 13:42:07 -0300

    '-l' now supports multiple args and returns an error not, when an unfound entry is given

--- optionFunctions.py
+++ optionFunctions.py
@@ -28,19 +28,24 @@ def list_entries():
 	path: str = setup.dict_from_parser()['PATHS']['data_file']
 
 	if os.stat(path).st_size == 0:
-		print("No entries found")
+		print("There are no entries in the data file")
 	elif len(sys.argv) > 2:
 		args: list = []
 
 		for i in range(2, len(sys.argv), 1):
-			args.append(sys.argv[i])
+			args.append(sys.argv[i].upper())
 			
 		py_dict = entry.get_json(path)
 		for key in py_dict:
 			if key in args:
+				args.remove(key)
 				print("Entries for " + key + ":" + '\n\n' \
 				+ json.dumps(py_dict[key], indent=2, sort_keys=True) + '\n\n')
-	 
+
+		unfound: int = len(args)
+		if unfound != 0:
+			for foo in args:
+				print(foo + " was not found among the entries" + '\n')
 	else:
 		py_dict: dict = entry.get_json(path)
 		print(json.dumps(py_dict, indent=2, sort_keys=True))
