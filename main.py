07e4da98f7cc75c812532220f6ce69bb1017f507
GvsCou <gvscou@protonmail.com>
2021-03-01 08:57:28 -0300

    Added possibility for '-l' to display only  an specific asset

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ def switch(option: str):
 	def list_entries():
 		if os.stat(paths['data_file']).st_size == 0:
 			print("No entries found")
+		elif len(sys.argv) > 3:
+			print("Too many arguments")
+		elif len(sys.argv) > 2:
+			py_dict: dict = entry.get_json(paths['data_file'])[sys.argv[2]]
+			print("Entries for " + sys.argv[2] + ":" + '\n\n' + json.dumps(py_dict, indent=2, sort_keys=True)) 
 		else:
 			py_dict: dict = entry.get_json(paths['data_file'])
 			print(json.dumps(py_dict, indent=2, sort_keys=True))
