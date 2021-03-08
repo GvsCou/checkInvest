4584cf671be69bf0ba10bb773bdb1ac7df3b3176
GvsCou <gvscou@protonmail.com>
2021-03-08 10:16:34 -0300

    Fixed 'qtd' var summing up the quantity of the last ticker

---
+++ scriptsOpt/listModes.py
@@ -0,0 +1,19 @@
+import sys, json
+import setup
+from . import entry
+
+def table_mode():
+	path: str = setup.dict_from_parser()['DATA_SET']['current']
+	py_dict: dict = entry.get_json(path)
+	
+	print("Ticker" + '\t' + "Quantity")
+	for ticker in py_dict:
+		qtd: float = 0.0
+		for key in py_dict[ticker]:
+			qtd += py_dict[ticker][key].get('quantity', 0.0)
+		print(ticker + '\t' + str(qtd))
+
+def json_mode():
+	path: str = setup.dict_from_parser()['DATA_SET']['current']
+	py_dict: dict = entry.get_json(path)
+	print(json.dumps(py_dict, indent=2, sort_keys=True))
