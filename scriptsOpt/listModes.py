06f1fadb32c4d98bbbb9364454e650ee0242ec5d
GvsCou <gvscou@protonmail.com>
2021-03-09 18:30:08 -0300

    Added 'yahooquery' and fixed '.sa' problem with brazillian assets

--- scriptsOpt/listModes.py
+++ scriptsOpt/listModes.py
@@ -1,8 +1,27 @@
 import sys, json
 import configOptions, cryptonator
+from yahooquery import Ticker
 from . import entry
 
-def table_mode(tickers: list):
+def table_mode(tickers: list) -> str:
+	def get_non_crypto(ticker, base_currency):
+		asset: Ticker = Ticker(ticker)
+		#Appends '.sa' if the stock is brazillian
+		if "Quote not found for ticker symbol: {}".format(ticker.upper()) in asset.price[ticker]:
+			ticker += ".sa"
+			asset = Ticker(ticker)		
+
+		given_currency: str = asset.price[ticker].get('currency', "")
+		if given_currency != base_currency:
+			asset_price: float = 0.0  if given_currency not in cryptonator.get_available_currencies() \
+			else cryptonator.get_exchange_rate(given_currency.lower(), currency.lower()) * \
+			asset.price[ticker].get('regularMarketPrice', 0.0)
+		else:
+			asset_price: float = asset.price[ticker].get('regularMarketPrice', 0.0)
+
+		return asset_price
+
+
 	path: str = configOptions.dict_from_parser()['DATA_SET']['current']
 	py_dict: dict = entry.get_json(path)
 	found: bool = True
@@ -25,7 +44,8 @@ def table_mode(tickers: list):
 		if tickers and ticker not in tickers:
 			continue 
 		qtd: float = 0.0
-		price: float = 0.0 if ticker not in cryptonator.get_available_currencies() \
+		price: float = float(get_non_crypto(ticker, currency)) \
+	 	if ticker not in cryptonator.get_available_currencies() \
 		else cryptonator.get_exchange_rate(ticker.lower(), currency.lower())
 		value: float = 0.0
 		for key in py_dict[ticker]:
